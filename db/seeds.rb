56de4e4d9c85c9bd64bd1817831e1433b0141361
Rashi Phuloria <rashiph@thoughtworks.com>
2012-11-27 10:54:57 +0530

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-pg_user.create([{usename: 'CampusHunter'}, {passwd: '!abcd1234'}])
